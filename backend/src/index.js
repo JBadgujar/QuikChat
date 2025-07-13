5cf0f45019c247eeb5e6e1b42b3b908e59189705
Jayesh Badgujar <badgujarjayesh02@gmail.com>
2025-07-14 00:43:53 +0530

    Project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,11 +32,11 @@ if(process.env.NODE_ENV === "production"){
 
     app.get("*", (req,res)=>{
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-        
-    })
+
+    });
 }
 
-server.listen(5001, () => {
+server.listen(PORT, () => {
     console.log("Server is running on port:"+ PORT)
     connectDB();
 })
